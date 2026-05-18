c5e7980588138de1c70581123803a9918cb9475f
mrtmrtmrtmrt <marat.yamalov@gmail.com>
2026-05-18 13:22:17 +0300

    Update Very_Secret.cpp

--- Very_Secret/Very_Secret/Very_Secret.cpp
+++ Very_Secret/Very_Secret/Very_Secret.cpp
@@ -7,13 +7,7 @@ int main()
 {
 	int x;
 	int a = 10;
-	cin >> x;
-	if (x == 0)
-	{
-		cout << 10 / x << endl;
-	}
-	while (x < a)
-	{
+
 		cout << x << " всё ещё меньше чем " << a << endl;
 		x++;
 	}
